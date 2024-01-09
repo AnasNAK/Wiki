6401167f52d43c6a96820b00110c1db9b85b638d
AnasNAK <anasc00ding@gmail.com>
2024-01-09 10:49:46 +0100

    create the signup page

--- app/views/pages/signup.php
+++ app/views/pages/signup.php
@@ -1,46 +1,71 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="scroll-smooth">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/a4fc922de4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <title>
+        <?php echo SITENAME ?>
+    </title>
 </head>
 
-<body class="bg-black text-yellow-500 p-8 rounded-md w-full text-center flex flex-col items-center justify-center">
-    <div class="bg-gray-700 p-8 rounded shadow-md w-full max-w-xl text-center">
+<body class="" style="background: rgb(102,102,102);
+background: radial-gradient(circle, rgba(102,102,102,1) 0%, rgba(250,246,246,1) 45%, rgba(0,0,0,1) 100%);">
 
-        <h1 class="text-2xl mb-4 text-green-500 mx-auto">Please Signup</h1>
-
-        <form method="post" action="<?php echo URLROOT ?>/Users/register" class="flex flex-col">
-
-            <input type="hidden" name="type" value="register" class="hidden">
-
-            <input type="text" name="username" placeholder="Full name..."
-                class="p-2 mb-2 border rounded bg-gray-900 text-white">
-
-            <input type="text" name="email" placeholder="Email..."
-                class="p-2 mb-2 border rounded bg-gray-900 text-white">
-
-            <input type="password" name="password" placeholder="Password..."
-                class="p-2 mb-2 border rounded bg-gray-900 text-white">
-
-            <input type="password" name="pwdRepeat" placeholder="Repeat password"
-                class="p-2 mb-2 border rounded bg-gray-900 text-white">
-
-            <!-- Select for Client and Artist -->
-            <label for="clientArtist" class="text-white mb-2">Select Client or Artist:</label>
-            <select name="role_type" id="clientArtist" class="p-2 mb-4 border rounded bg-gray-900 text-white">
-                <option value="1">Artist</option>
-                <option value="2">Client </option>
-
-            </select>
-
-            <button type="submit" name="submit" class="bg-green-600 text-white p-2 rounded cursor-pointer">Sign
-                Up</button>
-        </form>
+    <div class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+            <div class="absolute inset-0 bg-gradient-to-b from-black to-grey-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+            </div>
+            <div class="relative px-4 py-7 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                <div class="max-w-md mx-auto">
+                    <div>
+                        <h1 class="text-2xl font-semibold text-gray-800">Welcome Back! You Can Register Here</h1>
+                    </div>
+                    <form action="<?php echo URLROOT ?>/Users/login" method="post">
+                        <div class="divide-y divide-gray-400">
+                            <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                <div class="relative">
+                                    <input autocomplete="off" id="username" name="username" type="text"
+                                        class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-black focus:outline-none focus:border-rose-600"
+                                        placeholder="username" value="">
+                                    <label for="username"
+                                        class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
+                                        Username</label>
+                                </div>
+                                <div class="relative">
+                                    <input autocomplete="off" id="email" name="email" type="text"
+                                        class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-black focus:outline-none focus:border-rose-600"
+                                        placeholder="Email address" value="">
+                                    <label for="email"
+                                        class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email
+                                        Address</label>
+                                </div>
+                                <div class="relative">
+                                    <input autocomplete="off" id="password" name="password" type="password"
+                                        class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-black focus:outline-none focus:border-rose-600"
+                                        placeholder="Password" value="">
+                                    <label for="password"
+                                        class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                </div>
+                                <div class="relative flex flex-col gap-3">
+                                    <button
+                                        class="bg-gray-800 text-white rounded-md px-2 py-1 w-fit duration-300 hover:scale-x-125 hover:scale-y-110 duration-300 hover:bg-grey-600 hover:font-bold">Submit</button>
+                                    <a class="text-end font-bold duration-500 hover:text-grey-600"
+                                        href="<?= URLROOT . '/pages/index' ?>">Back to home</a>
+                                    <p class="text-sm">If you Already have an account! You can <a
+                                            class="text-end font-bold duration-500 hover:text-green-600"
+                                            href="<?= URLROOT . '/pages/login' ?>">Login Here</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</body>
 
-</html>
+</body>
