2740a8815121a06cb42d8b30fb2042cea8545285
AnasNAK <anasc00ding@gmail.com>
2024-01-12 16:34:04 +0100

    update in signup view

--- app/views/pages/signup.php
+++ app/views/pages/signup.php
@@ -13,19 +13,29 @@
     </title>
 </head>
 
-<body class="" style="background: rgb(102,102,102);
-background: radial-gradient(circle, rgba(102,102,102,1) 0%, rgba(250,246,246,1) 45%, rgba(0,0,0,1) 100%);">
+<body class="" style="background: #304352;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #d7d2cc, #304352);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #d7d2cc, #304352); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+overflow-x:hidden;">
+
+    <div id="flash-message" class="hidden bg-red-500 text-white z-50 p-4 fixed top-0 left-0 right-0 text-center">
+
+    </div>
 
     <div class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
         <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div class="absolute inset-0 bg-gradient-to-b from-black to-grey-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-black to-grey-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
             </div>
             <div class="relative px-4 py-7 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+
                 <div class="max-w-md mx-auto">
                     <div>
                         <h1 class="text-2xl font-semibold text-gray-800">Welcome Back! You Can Register Here</h1>
                     </div>
-                    <form action="<?php echo URLROOT ?>/Users/login" method="post">
+                    <?php flash('register') ?>
+                    <form action="<?php echo URLROOT ?>/Users/register" method="POST" id="myForm">
                         <div class="divide-y divide-gray-400">
                             <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                 <div class="relative">
@@ -52,7 +62,7 @@ background: radial-gradient(circle, rgba(102,102,102,1) 0%, rgba(250,246,246,1) 
                                         class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                                 </div>
                                 <div class="relative flex flex-col gap-3">
-                                    <button
+                                <button type="submit"
                                         class="bg-gray-800 text-white rounded-md px-2 py-1 w-fit duration-300 hover:scale-x-125 hover:scale-y-110 duration-300 hover:bg-grey-600 hover:font-bold">Submit</button>
                                     <a class="text-end font-bold duration-500 hover:text-grey-600"
                                         href="<?= URLROOT . '/pages/index' ?>">Back to home</a>
@@ -67,5 +77,8 @@ background: radial-gradient(circle, rgba(102,102,102,1) 0%, rgba(250,246,246,1) 
             </div>
         </div>
     </div>
+    <script src="<?php echo URLROOT ?>/js/signup.js"></script>
 
 </body>
+
+</html>
